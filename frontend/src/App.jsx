52d520e34860d3784f692f769524a107f560fe95
JasmondWorks <obafemilared@gmail.com>
2025-05-30 14:42:01 +0100

    set up the color scheme and typography

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import Welcome from "./pages/welcome_page";
 import NotFound from "./pages/not_found";
 import Chat from "./pages/chat_page";
 import Medications from "./pages/medications_page";
+import Onboarding from "./pages/onboarding";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route element={<Onboarding />} path="/onboarding" />
         <Route element={<Welcome />} path="/" />
         <Route element={<Chat />} path="/chat" />
         <Route element={<Medications />} path="/medications" />
--- frontend/src/pages/patients_home_page.jsx
+++
@@ -1,5 +0,0 @@
-function PatientsHomePage() {
-  return <div>Patients home page</div>;
-}
-
-export default PatientsHomePage;
