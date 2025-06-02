380ae8b3282b6f67506e91e7c20ddeba7e54125f
Seidu Boluwatife <seiduboluwatife@gmail.com>
2025-06-02 22:52:57 +0100

    mediaction-page-and-modals-ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,12 @@ import Welcome from "./pages/welcomePage";
 import ChatWelcome from "./pages/chatWelcomePage";
 import { BrowserRouter as Router , Routes ,Route } from 'react-router-dom'
 import AiCall from "./pages/aiCall";
+import Medication from "./pages/medication";
+import CheckUp from "./modalsUi/checkUp";
+import FoodCheck from "./modalsUi/foodCheck";
+import Suggestion from "./modalsUi/suggestion";
+import Congrats from "./modalsUi/congrats";
+import Prescription from "./modalsUi/prescribtion";
 function App() {
   return (
     <>
@@ -10,6 +16,13 @@ function App() {
           <Route path ="/" element ={<Welcome/>}/>
           <Route path ="/chat-welcome" element ={<ChatWelcome/>}/>
           <Route path ="/call-ai" element={<AiCall/>}/>
+          <Route path = "/medication" element ={<Medication/>}/>
+          <Route path = "/feeling-modal" element={<CheckUp/>}/>
+          <Route path="/eating-check-modal" element={<FoodCheck/>}/>
+          <Route path ="/suggestion-modal" element={<Suggestion/>}/>
+          <Route path ="/prescribtion-modal" element={<Prescription/>}/>
+          <Route path ="/congrats-modal" element={<Congrats/>}/>
+          
         </Routes>
       </Router>
 
