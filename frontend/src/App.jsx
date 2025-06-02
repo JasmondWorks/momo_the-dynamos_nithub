0edcd21e48a86542b6adf1b3086efb4768e2ce04
JasmondWorks <obafemilared@gmail.com>
2025-06-02 16:24:23 +0100

    created splash screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import Welcome from "./pages/welcomePage";
 import ChatWelcome from "./pages/chatWelcomePage";
-import { BrowserRouter as Router , Routes ,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AiCall from "./pages/aiCall";
+import SplashScreen from "./component/splashScreen";
 function App() {
   return (
     <>
-      <Router>
+      {/* <Router>
         <Routes>
           <Route path ="/" element ={<Welcome/>}/>
           <Route path ="/chat-welcome" element ={<ChatWelcome/>}/>
           <Route path ="/call-ai" element={<AiCall/>}/>
         </Routes>
-      </Router>
-
+      </Router> */}
+      <SplashScreen />
     </>
   );
 }
