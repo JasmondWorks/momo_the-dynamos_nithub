01d09e6cdf49d597d268f42813f00bb732ac90b7
JasmondWorks <obafemilared@gmail.com>
2025-06-03 12:21:17 +0100

    finished the chat flow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Welcome from "./pages/welcome_page";
+import Welcome from "./pages/welcomePage";
 import NotFound from "./pages/not_found";
 import Chat from "./pages/chat_page";
+import ChatWelcome from "./pages/chatWelcomePage";
 import Medications from "./pages/medications_page";
 import Onboarding from "./pages/onboarding";
+import AiCall from "./pages/aiCall";
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
       <Routes>
         <Route element={<Onboarding />} path="/onboarding" />
         <Route element={<Welcome />} path="/" />
-        <Route element={<Chat />} path="/chat" />
+        <Route element={<Chat />} path="/chats/:chatId" />
+        <Route element={<AiCall />} path="/chats/:chatId/call" />
+        <Route element={<ChatWelcome />} path="/chat-welcome" />
         <Route element={<Medications />} path="/medications" />
         <Route element={<NotFound />} path="*" />
       </Routes>
--- frontend/src/pages/chatWelcomePage.jsx
+++ frontend/src/pages/chatWelcomePage.jsx
@@ -1,31 +1,94 @@
-import WelcomeFrame from "../assets/welcomeFrame.svg"
-import Input from "../component/inputField"
-import userFrame from "../assets/userFrame.svg"
-import messageFrame from "../assets/message.svg"
-import SuggestionBox from "../component/suggestionBox"
+import WelcomeFrame from "../assets/welcomeFrame.svg";
+import Input from "../component/inputField";
+import userFrame from "../assets/userFrame.svg";
+import messageFrame from "../assets/message.svg";
+import SuggestionBox from "../component/suggestionBox";
+import phoneIcon from "../assets/phoneIcon.svg";
+import goBackIcon from "../assets/goBackIcon.svg";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-function ChatWelcome({name}){
-    return(
-        <div className="w-full h-100 text-center flex flex-col items-center">
-            <img src={WelcomeFrame} alt="welcome frame" />
-            <p>Welcome {name}</p>
-            <p>I'm here to help you with medical questions, symptom checks, and general health advice. </p>
-                <p>How can I assist you today?</p>
- 
-             <Input 
-             type='text'
-             placeholder=" Enter a Response "
-             rightIcon ={<img src={userFrame}/>}
-             button={<button type="submit">Send</button>}
-             />
+function ChatWelcome({ name = "Bolu" }) {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
 
-             <p className="mt-12 mb-2 text-[#646368]">Suggestions</p>
- 
-             <div className="flex justify-between pr-5 pl-5 gap-4 ">
-                <SuggestionBox img ={messageFrame} name ='Headache'/>
-                <SuggestionBox img ={messageFrame} name ='Fever and cold'/>
-                <SuggestionBox img ={messageFrame} name ='Back Pain causes'/> 
-             </div>
+  console.log(input);
+
+  function handleInputChange(e) {
+    setInput(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!input) return;
+
+    navigate("/chats/abc");
+  }
+
+  return (
+    <>
+      <div className="p-6 lg:p-12 h-[100vh] max-w-5xl mx-auto text-sm bg-[#fdfcfc]">
+        <div className="p-5 border border-neutral-400 rounded-lg h-full grid grid-rows-[auto_1fr] gap-5">
+          <header className="flex justify-between items-center">
+            <img src={goBackIcon} alt="back icon" className="h-4" />
+            <Link
+              to="/chats/abc/call"
+              className="bg-[var(--color-primary)] rounded-full p-2 cursor-pointer"
+            >
+              <img
+                src={phoneIcon}
+                alt="phone icon"
+                className="aspect-square h-4"
+              />
+            </Link>
+          </header>
+          <div className="flex items-center justify-center">
+            <div className="flex flex-col items-center gap-12">
+              <div className="flex flex-col items-center gap-3">
+                <img src={WelcomeFrame} alt="welcome frame" className="h-16" />
+                <p className="text-lg leading-4">Welcome {name}</p>
+                <p className="text-center text-sm text-[#646368]">
+                  I'm here to help you with medical questions, symptom checks,
+                  and general health advice. <br />
+                  How can I assist you today?
+                </p>
+                <p></p>
+              </div>
+              <form
+                onSubmit={handleSubmit}
+                className="flex flex-col items-center gap-3"
+              >
+                <Input
+                  value={input}
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder=" Enter a Response "
+                  rightIcon={<img src={userFrame} />}
+                  button={
+                    <button
+                      type="submit"
+                      disabled={!input}
+                      className="disabled:cursor-not-allowed disabled:opacity-40 p-2 rounded-md text-white bg-[#8E44AD] cursor-pointer"
+                    >
+                      Send
+                    </button>
+                  }
+                />
+                <p className="text-[#646368] text-xs font-semibold !mt-5">
+                  Suggestions
+                </p>
+                <div className="flex flex-wrap items-center justify-center pr-5 pl-5 gap-4 ">
+                  <SuggestionBox img={messageFrame} name="Headache" />
+                  <SuggestionBox img={messageFrame} name="Fever and cold" />
+                  <SuggestionBox img={messageFrame} name="Back Pain causes" />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-    )
-}export default ChatWelcome
+      </div>
+    </>
+  );
+}
+export default ChatWelcome;
