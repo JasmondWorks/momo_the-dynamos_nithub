fcf59f280b0fa8193fc26c11b1e8c0f17dabbeb6
JasmondWorks <obafemilared@gmail.com>
2025-06-03 17:04:58 +0100

    modified the welcome page ui

--- frontend/src/component/boxes.jsx
+++ frontend/src/component/boxes.jsx
@@ -1,13 +1,21 @@
 import { ChevronRight } from "lucide-react";
+import { Link } from "react-router-dom";
 
-function Box({ img, name }) {
+function Box({ img, name, href = "#" }) {
   return (
     <div>
       <div>
-        <img src={img} alt=" AN IMAGE" className="w-full" />
-        <div className="flex justify-between gap- border-1 border-[#8E44AD] h-max p-2 text-[#8E44AD] text-lg">
+        <img
+          src={img}
+          alt=" AN IMAGE"
+          className="w-full rounded-tl-xl rounded-tr-xl"
+        />
+        <Link
+          to={href}
+          className="bg-white flex items-center font-medium justify-between gap- border-1 border-[#8E44AD] border-t-transparent hover:bg-[#8E44AD] hover:text-white h-max p-3 text-[#8E44AD] text-sm rounded-bl-xl rounded-br-xl transition-all duration-300"
+        >
           {name} {<ChevronRight />}
-        </div>
+        </Link>
       </div>
     </div>
   );
--- frontend/src/pages/not_found.jsx
+++ frontend/src/pages/not_found.jsx
@@ -1,22 +1,25 @@
+import { Link } from "react-router-dom";
 import MomoLogo from "/src/assets/momoLogo.svg";
 
 function NotFound() {
   return (
     <div className="h-screen text-center grid grid-rows-[auto_1fr]">
       {/* Navbar */}
-      <div className="flex gap-3 items-center shadow px-4 py-2 max-w-5xl">
-        <img src={MomoLogo} alt="logo" className="h-10" />
-        <h1 className="font-bold">Momo</h1>
+      <div className="shadow px-4 py-2 ">
+        <Link to="/" className="flex gap-3 items-center w-fit">
+          <img src={MomoLogo} alt="logo" className="h-10" />
+          <h1 className="font-bold">Momo</h1>
+        </Link>
       </div>
-      <div className="space-y-5 h-full flex flex-col justify-center">
-        <p>This page could not be found</p>
+      <div className="space-y-5 h-full flex flex-col justify-center p-5">
+        <h3 className="text-2xl">This page could not be found!</h3>
         <div>
-          <a
-            href="/"
-            className="p-4 py-2 inline-block bg-[#8e44ad] font-bold text-white"
+          <Link
+            to="/"
+            className="p-4 py-3 inline-block bg-[#8e44ad] font-bold text-white rounded-md"
           >
             Back to Home
-          </a>
+          </Link>
         </div>
       </div>
     </div>
